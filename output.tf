fc0c61c6eb23118ad54a60080ccd757b8640a775
Noah Kitisya Makau <nkmakau@safaricom.co.ke>
2022-11-01 16:08:25 +0300

    edited outputs

--- output.tf
+++ output.tf
@@ -2,15 +2,15 @@
 
 output "arn" {
   description = "ARN of the bucket"
-  value       = module.s3_website.arn
+  value       = module.s3_website.arn.arn
 }
 
 output "name" {
   description = "Name (id) of the bucket"
-  value       = module.s3_website.id
+  value       = module.s3_website.name.id
 }
 
 output "domain" {
   description = "Domain name of the bucket"
-  value       = module.s3_website.website_domain
+  value       = module.s3_website.domain.website_domain
 }
